e7fdeedfe00c438863a4562b65d1be84d70726e1
Panuwat <netpopo1150@outlook.com>
2024-01-22 11:40:23 +0700

    using useState in input

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState('Greeting Message');
+  const [text, setText] = useState("");
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input 
+          id="greeting-message" 
+          type="text" 
+          value={(text)}
+          onChange={(event) => setText(event.target.value)}/>
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={() => setMessage(text)}>Update text</button>
       </div>
     </div>
   );
